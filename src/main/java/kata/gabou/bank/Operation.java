089ebe45ada00c0d4552c4abecf8e5834c912db2
Gabou <supergabou+github@outlook.fr>
2020-10-06 14:58:17 +0200

    Add date to history operations

--- src/main/java/kata/gabou/bank/Operation.java
+++ src/main/java/kata/gabou/bank/Operation.java
@@ -1,15 +1,24 @@
 package kata.gabou.bank;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Operation {
 
     private final OperationType type;
     private final Amount amount;
+    private final LocalDate date;
 
     public Operation(OperationType type, Amount amount) {
         this.type = type;
         this.amount = amount;
+        date = LocalDate.now();
+    }
+
+    public Operation(OperationType type, Amount amount, LocalDate date) {
+        this.type = type;
+        this.amount = amount;
+        this.date = date;
     }
 
     public OperationType type() {
@@ -26,12 +35,13 @@ public class Operation {
         if (o == null || getClass() != o.getClass()) return false;
         Operation operation = (Operation) o;
         return type == operation.type &&
-                Objects.equals(amount, operation.amount);
+                Objects.equals(amount, operation.amount) &&
+                Objects.equals(date, operation.date);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(type, amount);
+        return Objects.hash(type, amount, date);
     }
 
     @Override
@@ -39,6 +49,7 @@ public class Operation {
         return "Operation{" +
                 "type=" + type +
                 ", amount=" + amount +
+                ", date=" + date +
                 '}';
     }
 }
--- src/test/java/kata/gabou/bank/HistoryShould.java
+++ src/test/java/kata/gabou/bank/HistoryShould.java
@@ -3,6 +3,7 @@ package kata.gabou.bank;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,6 +19,19 @@ public class HistoryShould {
         history.add(new Operation(WITHDRAWAL, new Amount(4,0)));
 
         List<Operation> expected = Arrays.asList(new Operation(DEPOSIT, new Amount(5,0)), new Operation(WITHDRAWAL, new Amount(4,0)));
+        Assertions.assertThat(history.list()).isEqualTo(expected);
+    }
+
+    @Test
+    void keep_operations_with_date() {
+        AccountHistory history = new AccountHistory();
+        history.add(new Operation(DEPOSIT, new Amount(5,0),LocalDate.of(2020,8,21)));
+        history.add(new Operation(WITHDRAWAL, new Amount(4,0),LocalDate.of(2020,8,24)));
+
+        List<Operation> expected = Arrays.asList(
+                new Operation(DEPOSIT, new Amount(5,0),LocalDate.of(2020,8,21)),
+                new Operation(WITHDRAWAL, new Amount(4,0),LocalDate.of(2020,8,24))
+        );
         Assertions.assertThat(history.list()).isEqualTo(expected);
     }
 }
